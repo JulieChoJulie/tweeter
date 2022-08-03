5059dc371cf730af2226ec2447820a66ae29241c
JulieChoJulie <h32cho@gmail.com>
2022-08-03 16:22:51 -0700

    hash tag -back added

--- back/routes/post.js
+++ back/routes/post.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 const router = express.Router();
-const { Post, Comment, Image, User } = require('../models');
+const { Post, Comment, Image, User, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 try {
@@ -30,10 +30,18 @@ const upload = multer({
 
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
     try {
+        const hashtags = req.body.content.match(/#[^\s#]+/g);
         const post = await Post.create({
             content: req.body.content,
             UserId: req.user.id,
         });
+        
+        if (hashtags) {
+            const result = await Promise.all(hashtags.map((tag) => Hashtag.findOrCreate({ 
+                where: { name: tag.slice(1).toLowerCase() } 
+            }))); // [[hello, true], [react, false]]
+            await post.addHashtags(result.map((h) => h[0]));
+        }
 
         if (req.body.image) {
             if (Array.isArray(req.body.image)) {
